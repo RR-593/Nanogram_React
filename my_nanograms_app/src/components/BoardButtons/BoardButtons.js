d17a5145625e7419598d25fbd9952ff04c3506f8
RR-593 <Raphael@Munoz.to>
2024-11-04 18:07:30 +1100

    Add check board button

--- my_nanograms_app/src/components/BoardButtons/BoardButtons.js
+++ my_nanograms_app/src/components/BoardButtons/BoardButtons.js
@@ -1,19 +1,37 @@
 import { useEffect, useState, useContext } from 'react'
 import {useStatsContext} from '../StatsProvider'
+import './BoardButtons.css'
 
 const BoardButtons = (props) => {
   const [game_stats, updateGStats] = useStatsContext();
 
-  let clearBoard = ()=>{
+  let clearBoardAction = ()=>{
     updateGStats({...game_stats, board_count: game_stats.board_count+1})
   }
+
+  let submitBoardAction = ()=>{
+    
+  }
+
+  let clearBoardB = (
+    <button onClick={clearBoardAction}>
+      <span>
+        Clear Board
+      </span>
+    </button>
+  )
+
+  let submitBoardB = (
+    <button onClick={submitBoardAction}>
+      <span>
+        Check Board
+      </span>
+    </button>
+  )
   return(
     <div className="boardButtBox">
-      <button onClick={clearBoard}>
-        <span>
-          Clear Board
-        </span>
-      </button>
+      {clearBoardB}
+      {submitBoardB}
     </div>
   )
 }
