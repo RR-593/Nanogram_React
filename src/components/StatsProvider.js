85d164773ff92384efdfdfb64968b05ebd3ce1fc
RR-593 <Raphael@Munoz.to>
2024-11-07 13:17:35 +1100

    Fix save feature

--- src/components/StatsProvider.js
+++ src/components/StatsProvider.js
@@ -8,14 +8,15 @@ const StatsProvider = ({ children }) => {
 		complete_puzzle: false,
 		default_board_size: 4,
 		max_board_size: 25,
-		numberOfCompletions: {},
+		numberOfCompletions: {}
   })
   const updateGStats = (newValue) => {
     setGStats(newValue);
   };
 
 	useEffect(() => {
-		const got_stats = JSON.parse(localStorage.getItem('game_stats'));
+		const got_stats = JSON.parse(localStorage.getItem('stats'));
+		got_stats.complete_puzzle = false
 		if (got_stats) setGStats(got_stats);
 	}, []);
 
