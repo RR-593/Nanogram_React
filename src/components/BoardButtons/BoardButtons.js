1d2a9dcd9131a96d4e3fe000339f92dea4921989
RR-593 <Raphael@Munoz.to>
2024-11-08 18:50:50 +1100

    Add difficulty buttons

--- src/components/BoardButtons/BoardButtons.js
+++ src/components/BoardButtons/BoardButtons.js
@@ -1,56 +1,56 @@
 import { useEffect, useState, useContext } from 'react'
 import $ from 'jquery'
-import {useStatsContext} from '../Providers/StatsProvider.js'
-import {useNanogramContext} from '../Providers/NanogramProvider.js'
-import {useActiveBoardContext} from '../Providers/ActiveBoardProvider.js'
+import { useStatsContext } from '../Providers/StatsProvider.js'
+import { useNanogramContext } from '../Providers/NanogramProvider.js'
+import { useActiveBoardContext } from '../Providers/ActiveBoardProvider.js'
 import './BoardButtons.css'
 
-import {compareNanograms, toString2DArray} from '../helper_funcs/arrayFunctions.js'
+import { compareNanograms, toString2DArray } from '../helper_funcs/arrayFunctions.js'
 
 const BoardButtons = (props) => {
-  
+
   const [game_stats, updateGStats] = useStatsContext();
   const [nanogram, setNewNanogram] = useNanogramContext();
   const [board, updateBoard] = useActiveBoardContext();
 
-  let clearBoardAction = ()=>{
-    updateGStats({...game_stats, clear: !game_stats.clear})
+  let clearBoardAction = () => {
+    updateGStats({ ...game_stats, clear: !game_stats.clear })
   }
 
-  let submitBoardAction = ()=>{
-    let isNonoCorrect = compareNanograms(board,nanogram.nanogramArr)
-    if(!isNonoCorrect) return
-    updateGStats({...game_stats, complete_puzzle: compareNanograms(board,nanogram.nanogramArr)})
+  let submitBoardAction = () => {
+    let isNonoCorrect = compareNanograms(board, nanogram.nanogramArr)
+    if (!isNonoCorrect) return
+    updateGStats({ ...game_stats, complete_puzzle: compareNanograms(board, nanogram.nanogramArr) })
     // console.log(compareNanograms(board,nanogram.nanogramArr)?"Correct!":"Incorrect")
     // console.log(toString2DArray(board)+"\n\n"+toString2DArray(nanogram.nanogramArr))
   }
 
-  let newBoardAction = ()=>{
+  let newBoardAction = () => {
     let getInput = Number($("input[name='input_board_size']").val())
-    getInput = getInput > 25 ? 25 : 
-    typeof getInput !== "undefined" && getInput > game_stats.default_board_size ? getInput : 
-    game_stats.default_board_size
+    getInput = getInput > 25 ? 25 :
+      typeof getInput !== "undefined" && getInput > game_stats.default_board_size ? getInput :
+        game_stats.default_board_size
 
     $("input[name='input_board_size']").val(getInput)
 
-    updateGStats({...game_stats, complete_puzzle: false})
-    setNewNanogram({size: getInput})
+    updateGStats({ ...game_stats, complete_puzzle: false })
+    setNewNanogram({ size: getInput })
   }
 
   let newBoardB = (
     <div className="newBoardInputs">
-    <input name="input_board_size" type="text" placeholder={game_stats.default_board_size}></input>
-    <button onClick={newBoardAction}>
-      <span>
-        <i className="fa fa-plus fa-1x"></i>
-      </span>
-    </button>
+      <input name="input_board_size" type="text" placeholder={game_stats.default_board_size}></input>
+      <button onClick={newBoardAction}>
+        <span>
+          <i className="fa fa-plus fa-1x"></i>
+        </span>
+      </button>
     </div>
   )
 
   let clearBoardB = (
     <button onClick={clearBoardAction}>
-      <span style={{ display: "flex", justifyContent: "center", gap: "20px"}} >
+      <span style={{ display: "flex", justifyContent: "center", gap: "20px" }} >
         {/* <i class="fa fa-file-o fa-1x" ></i> */}
         Clear
       </span>
@@ -64,11 +64,29 @@ const BoardButtons = (props) => {
       </span>
     </button>
   )
-  return(
+
+  let difficultyButts = (
+    <div className="difficultyButts">
+      <label>
+        <input type="radio" name="difficulty" className="easy" />
+        <span></span><div id="text">🥞</div>
+      </label>
+      <label>
+        <input type="radio" name="difficulty" className="normal" />
+        <span></span><div id="text">🌗</div>
+      </label>
+      <label>
+        <input type="radio" name="difficulty" className="hard" />
+        <span></span><div id="text">🔥</div>
+      </label>
+    </div>)
+
+  return (
     <div className="boardButtBox">
       {newBoardB}
       {clearBoardB}
       {submitBoardB}
+      {difficultyButts}
     </div>
   )
 }
