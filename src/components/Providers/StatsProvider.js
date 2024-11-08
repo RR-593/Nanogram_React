f88ace18cb80baa316e444e6c7504ef15507d61c
RR-593 <Raphael@Munoz.to>
2024-11-08 20:01:14 +1100

    Update version number

--- src/components/Providers/StatsProvider.js
+++ src/components/Providers/StatsProvider.js
@@ -3,7 +3,7 @@ import { useState, createContext, useContext, useEffect } from 'react'
 const StatsContext = createContext()
 
 const StatsProvider = ({ children }) => {
-	const game_ver = "V1.2" // this is used to reset people data so nothing breaks, increment number for fresh reset
+	const game_ver = "V1.2.1" // this is used to reset people data so nothing breaks, increment number for fresh reset
 	const defualt_game_stats = {
 		version: game_ver,
 		save: false,
--- src/components/Unlocks/Version/Version.js
+++ src/components/Unlocks/Version/Version.js
@@ -23,13 +23,19 @@ const Version = () => {
 
 	const vLog = (
 		<div className="versionDiv">
-			<button className="verLogButt" onClick={()=>{ $('.versions').toggle() }}>Version Log</button>
+			<button className="verLogButt" onClick={() => { $('.versions').toggle() }}>Version Log</button>
 			<div className="versions">
-			<div className="version" id="1.2">
-					<h3>---  V1.1.2  ---</h3>
+			<div className="version" id="1.2.1">
+					<h3>---  V1.2.1  ---</h3>
 					<p>
 						New fetures:
 					</p>
+					<ul>
+						<li>Difficulty choice buttons are lock by currency </li>
+					</ul>
+				</div>
+				<div className="version" id="1.2">
+					<h3>---  V1.2  ---</h3>
 					<ul>
 						<li>Difficulty choice buttons </li>
 					</ul>
