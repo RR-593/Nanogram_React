aeb35527071611041220dfdcf5ca5e8bd1eef36e
RR-593 <Raphael@Munoz.to>
2024-11-16 18:01:07 +1100

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import { useEffect, useState } from 'react'
+import React from 'react';
 import './App.css';
+
+
+// import { GameProvider } from './contexts/GameContext';
+// import { TimerProvider } from './contexts/TimerContext';
+// import GameBoard from './components/GameBoard';
+// import Timer from './components/Timer';
+// import Scoreboard from './components/Scoreboard';
+// import GameControls from './components/GameControls';
 
 import StatsProvider from './components/Providers/StatsProvider'
 import NanogramProvider from './components/Providers/NanogramProvider';
@@ -13,6 +21,20 @@ import Confetti_Cone from './components/Confetti/Confetti_Cone'
 
 function App() {
   return (
+    // <GameProvider>
+    //   <TimerProvider>
+    //     <div className="App">
+    //       <header className="App-header">
+    //         <h1>Nonograms</h1>
+    //       </header>
+    //       <GameControls />
+    //       <GameBoard />
+    //       <Timer />
+    //       <Scoreboard />
+    //     </div>
+    //   </TimerProvider>
+    // </GameProvider>
+
     <div className="App">
       <header className="App-header">
         <h1>Nonograms</h1>
@@ -25,7 +47,7 @@ function App() {
               <div className="GameInterface">
                 <BoardButtons />
                 <DisplayNanoBoard />
-                <Version/>
+                <Version />
               </div>
               <Confetti_Cone />
             </div>
@@ -35,5 +57,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
